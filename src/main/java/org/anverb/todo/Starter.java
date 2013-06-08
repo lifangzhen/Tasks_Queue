08b24c94ac60b7588fee815a6c62677d7acae722
Anver Bogatov <bogatoff@glagol.giport.ru>
2013-06-09 00:30:03 +0400

    Initial commit.

---
+++ src/main/java/org/anverb/todo/Starter.java
@@ -0,0 +1,13 @@
+package org.anverb.todo;
+
+/**
+ * TODO: Place javadocs here
+ *
+ * @author Anver
+ * @version 0.1, 09.06.13
+ */
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("It's working!");
+    }
+}
